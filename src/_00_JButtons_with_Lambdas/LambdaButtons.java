0540dce9cf46c055e859d7888718928ab9521ca9
jfluo05 <jfluo05@users.noreply.github.com>
2021-06-01 19:59:19 -0700

    Update LambdaButtons.java

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,9 +1,12 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class LambdaButtons {
 	private JFrame window = new JFrame();
@@ -20,6 +23,32 @@ public class LambdaButtons {
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
+		
+		tellAJoke.addActionListener((ActionEvent e)->{
+			String jokeAnswer=JOptionPane.showInputDialog("What do you call a fake noodle?");
+			if (jokeAnswer.equalsIgnoreCase("impasta")||jokeAnswer.equalsIgnoreCase("an impasta")) {
+				JOptionPane.showMessageDialog(null,"yay!");
+			}else {
+				JOptionPane.showMessageDialog(null,"Wrong!");
+			}
+		});
+		addNumbers.addActionListener((ActionEvent e)->{
+			String number1= JOptionPane.showInputDialog("Pick a number:");
+			int num1= Integer.parseInt(number1);
+			String number2= JOptionPane.showInputDialog("Pick another number:");
+			int num2= Integer.parseInt(number2);
+			int num3=0;
+			
+			num3=num1+num2;
+			JOptionPane.showMessageDialog(null, "The sum of the two numbers you chose is: "+num3);
+			
+		});
+		randNumber.addActionListener((ActionEvent e)->{
+			Random r= new Random();
+			String number1= JOptionPane.showInputDialog("Choose a number: ");
+			int num1= Integer.parseInt(number1);
+			JOptionPane.showMessageDialog(null, r.nextInt(num1));
+		});
 		
 		window.setVisible(true);
 		window.pack();
